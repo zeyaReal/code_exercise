938f8463a7ad953af3864b6a963590fe74f48b48
zeya <zhangying_cd@hotmail.com>
2014-09-12 23:40:30 +0800

    重构

--- src/main/java/TexiMeter.java
+++ src/main/java/TexiMeter.java
@@ -5,30 +5,31 @@ public class TexiMeter {
 
     public static final int INITIAL_DISTANCE_KILOMETER = 2;
     public static final float INITIAL_FARE = 6.00f;
-    public static final float BASE_FARE_RATE = 1.50f;
+
+    public static final float BASE_FARE_PER_KILOMETER = 1.50f;
     public static final int BASE_DISTANCE_KILOMETER = 8;
+
+    public static final float SPECIAL_FARE_RATE = 1.50f;
 
     public float fareForDistance(float distanceKilometer) {
         if (distanceKilometer <= 0) {
             return 0;
         }
 
+        return INITIAL_FARE + countBasefare(distanceKilometer) + countSpecialFare(distanceKilometer);
+    }
 
-        float totalFare = INITIAL_FARE;
-        if (distanceKilometer <= INITIAL_DISTANCE_KILOMETER) {
-            return totalFare;
-        }
-
-        float baseFare = 0;
-        if (distanceKilometer > INITIAL_DISTANCE_KILOMETER) {
-             baseFare = (Math.min(distanceKilometer, BASE_DISTANCE_KILOMETER) - INITIAL_DISTANCE_KILOMETER) * BASE_FARE_RATE;
-        }
-
-        float specialFare = 0;
+    private float countSpecialFare(float distanceKilometer) {
         if (distanceKilometer > BASE_DISTANCE_KILOMETER) {
-            specialFare = (distanceKilometer - BASE_DISTANCE_KILOMETER) * BASE_FARE_RATE * 1.50f;
+            return (distanceKilometer - BASE_DISTANCE_KILOMETER) * BASE_FARE_PER_KILOMETER * SPECIAL_FARE_RATE;
         }
+        return 0;
+    }
 
-        return totalFare + baseFare + specialFare;
+    private float countBasefare(float distanceKilometer) {
+        if (distanceKilometer > INITIAL_DISTANCE_KILOMETER) {
+             return (Math.min(distanceKilometer, BASE_DISTANCE_KILOMETER) - INITIAL_DISTANCE_KILOMETER) * BASE_FARE_PER_KILOMETER;
+        }
+        return 0;
     }
 }

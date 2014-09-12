84243e3b06886b97026c91bc6f2fe193e1c1ffd8
zeya <zhangying_cd@hotmail.com>
2014-09-12 23:33:43 +0800

    重构

--- src/main/java/TexiMeter.java
+++ src/main/java/TexiMeter.java
@@ -6,19 +6,29 @@ public class TexiMeter {
     public static final int INITIAL_DISTANCE_KILOMETER = 2;
     public static final float INITIAL_FARE = 6.00f;
     public static final float BASE_FARE_RATE = 1.50f;
+    public static final int BASE_DISTANCE_KILOMETER = 8;
 
     public float fareForDistance(float distanceKilometer) {
         if (distanceKilometer <= 0) {
             return 0;
         }
+
+
+        float totalFare = INITIAL_FARE;
         if (distanceKilometer <= INITIAL_DISTANCE_KILOMETER) {
-            return INITIAL_FARE;
+            return totalFare;
         }
 
-        if (distanceKilometer <= 8) {
-            return INITIAL_FARE + (distanceKilometer - INITIAL_DISTANCE_KILOMETER) * BASE_FARE_RATE;
+        float baseFare = 0;
+        if (distanceKilometer > INITIAL_DISTANCE_KILOMETER) {
+             baseFare = (Math.min(distanceKilometer, BASE_DISTANCE_KILOMETER) - INITIAL_DISTANCE_KILOMETER) * BASE_FARE_RATE;
         }
 
-        return INITIAL_FARE + ( 8 - INITIAL_DISTANCE_KILOMETER ) * BASE_FARE_RATE + (distanceKilometer - 8) * BASE_FARE_RATE * 1.50f;
+        float specialFare = 0;
+        if (distanceKilometer > BASE_DISTANCE_KILOMETER) {
+            specialFare = (distanceKilometer - BASE_DISTANCE_KILOMETER) * BASE_FARE_RATE * 1.50f;
+        }
+
+        return totalFare + baseFare + specialFare;
     }
 }

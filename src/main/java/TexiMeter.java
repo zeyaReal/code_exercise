826c202f8ccb247bb4b3b7594df2c2896828a5e1
zeya <zhangying_cd@hotmail.com>
2014-09-12 22:44:50 +0800

    完成1.8公里计价

---
+++ src/main/java/TexiMeter.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zeya on 14-9-12.
+ */
+public class TexiMeter {
+
+    public float fareForDistance(float distanceKilometer) {
+        if (distanceKilometer <=0 )
+            return 0;
+        return 6;
+    }
+}
---
+++ src/test/java/TexiMeterTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by zeya on 14-9-12.
+ */
+public class TexiMeterTest {
+
+    @Test
+    public void test_fareForDistance_should_return_0_when_distance_is_0_kilometer() throws Exception {
+        TexiMeter texiMeter = new TexiMeter();
+        assertThat(texiMeter.fareForDistance(0f), is(0f));
+    }
+
+    @Test
+    public void test_fareForDistance_should_return_6_when_distance_is_1_8_kilometer() throws Exception {
+        TexiMeter texiMeter = new TexiMeter();
+        assertThat(texiMeter.fareForDistance(1.8f), is(6.00f));
+    }
+
+}

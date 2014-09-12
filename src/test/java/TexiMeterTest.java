c9142294ccfcdc79b3d5dc99282c02024ee02634
zeya <zhangying_cd@hotmail.com>
2014-09-12 22:45:40 +0800

    完成2公里计价

--- src/test/java/TexiMeterTest.java
+++ src/test/java/TexiMeterTest.java
@@ -20,4 +20,11 @@ public class TexiMeterTest {
         assertThat(texiMeter.fareForDistance(1.8f), is(6.00f));
     }
 
+    @Test
+    public void test_fareForDistance_should_return_6_when_distance_is_2_0_kilometer() throws Exception {
+        TexiMeter texiMeter = new TexiMeter();
+        assertThat(texiMeter.fareForDistance(2.0f), is(6.00f));
+    }
+
+
 }

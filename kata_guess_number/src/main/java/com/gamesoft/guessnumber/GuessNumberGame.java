5b693a32ae22e801a9ada17fb893d52aabdc36b0
zeya <zeyaxp@hotmail.com>
2014-09-23 22:59:12 +0800

    编写第一个测试并通过

---
+++ kata_guess_number/src/main/java/com/gamesoft/guessnumber/GuessNumberGame.java
@@ -0,0 +1,16 @@
+package com.gamesoft.guessnumber;
+
+/**
+ * Created by zeya on 14-9-23.
+ */
+public class GuessNumberGame {
+    private int[] answerNumbers;
+
+    public GuessNumberGame(int[] answerNumbers) {
+        this.answerNumbers = answerNumbers;
+    }
+
+    public String guess(int[] guessNumbers) {
+        return "1A0B";
+    }
+}
--- kata_guess_number/src/test/java/com/gamesoft/guessnumber/GuessNumberGameTest.java
+++ kata_guess_number/src/test/java/com/gamesoft/guessnumber/GuessNumberGameTest.java
@@ -1,7 +1,18 @@
 package com.gamesoft.guessnumber;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by zeya on 14-9-23.
  */
 public class GuessNumberGameTest {
+
+    @Test
+    public void test_should_return_1A0B_when_input_1567() throws Exception {
+        GuessNumberGame game = new GuessNumberGame(new int[]{1,2,3,4});
+        assertThat(game.guess(new int[]{1,5,6,7}), is("1A0B"));
+    }
 }

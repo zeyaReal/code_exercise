d0777b69caa89fb40aa5c051f4e9bf1b917a2d1b
zeya <zeyaxp@hotmail.com>
2014-09-24 19:08:37 +0800

    重构

--- kata_guess_number/src/test/java/com/gamesoft/guessnumber/GuessNumberGameTest.java
+++ kata_guess_number/src/test/java/com/gamesoft/guessnumber/GuessNumberGameTest.java
@@ -1,5 +1,6 @@
 package com.gamesoft.guessnumber;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,28 +11,31 @@ import static org.hamcrest.core.Is.is;
  */
 public class GuessNumberGameTest {
 
+    private GuessNumberGame game;
+
+    @Before
+    public void setUp() {
+        game = new GuessNumberGame(new int[]{1,2,3,4});
+    }
+
     @Test
     public void test_should_return_1A0B_when_input_1567() throws Exception {
-        GuessNumberGame game = new GuessNumberGame(new int[]{1,2,3,4});
         assertThat(game.guess(new int[]{1,5,6,7}), is("1A0B"));
     }
 
     @Test
     public void test_should_return_2A0B_when_input_1267() throws Exception {
-        GuessNumberGame game = new GuessNumberGame(new int[]{1,2,3,4});
         assertThat(game.guess(new int[]{1,2,6,7}), is("2A0B"));
     }
 
     @Test
     public void test_should_return_3A0B_when_input_1237() throws Exception {
-        GuessNumberGame game = new GuessNumberGame(new int[]{1,2,3,4});
         assertThat(game.guess(new int[]{1,2,3,7}), is("3A0B"));
     }
 
     @Test
     public void test_should_return_4A0B_when_input_1234() throws Exception {
-        GuessNumberGame game = new GuessNumberGame(new int[]{1,2,3,4});
-        assertThat(game.guess(new int[]{1,2,3,4}), is("4A0B"));
+        assertThat(game.guess(new int[]{1, 2, 3, 4}), is("4A0B"));
     }
 
 }

a46037012e93fc97b010aa348079a7bcc4592f66
zeya <zeyaxp@hotmail.com>
2014-09-26 23:35:09 +0800

    尝试下hamcrest其他的数组匹配判定写法

--- kata_guess_number/src/test/java/com/gamesoft/guessnumber/RandomAnswerGeneratorTest.java
+++ kata_guess_number/src/test/java/com/gamesoft/guessnumber/RandomAnswerGeneratorTest.java
@@ -1,7 +1,5 @@
 package com.gamesoft.guessnumber;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
@@ -10,11 +8,8 @@ import org.mockito.internal.util.reflection.Whitebox;
 import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -36,7 +31,8 @@ public class RandomAnswerGeneratorTest {
         //1.随机生成器不用类变量就必须用PowerMock么？
         //2.hamcrest中比数组的比较器是哪个？
 //        assertThat(gameAnswerGenerator.randomGenerate(),  array(new int[]{1, 2, 3, 4}));
-        assertArrayEquals(gameAnswerGenerator.randomGenerate(), new int[]{0, 1, 2, 3});
+        assertThat(gameAnswerGenerator.randomGenerate(), equalTo(new int[]{0, 1, 2, 3}));
+//        assertArrayEquals(gameAnswerGenerator.randomGenerate(), new int[]{0, 1, 2, 3});
 
         InOrder inOrder = Mockito.inOrder(mockRandom);
         inOrder.verify(mockRandom).nextInt(9);
@@ -55,8 +51,9 @@ public class RandomAnswerGeneratorTest {
         when(mockRandom.nextInt(7)).thenReturn(1);
         when(mockRandom.nextInt(6)).thenReturn(0);
 
-//        assertThat(gameAnswerGenerator.randomGenerate(),  array(new int[]{1, 2, 3, 4}));
-        assertArrayEquals(gameAnswerGenerator.randomGenerate(), new int[]{1, 0, 3, 2});
+//        assertThat(gameAnswerGenerator.randomGenerate(), array(is(new int[]{1, 0, 3, 2})));
+        assertThat(gameAnswerGenerator.randomGenerate(), equalTo(new int[]{1, 0, 3, 2}));
+//        assertArrayEquals(gameAnswerGenerator.randomGenerate(), new int[]{1, 0, 3, 2});
 
         InOrder inOrder = Mockito.inOrder(mockRandom);
         inOrder.verify(mockRandom).nextInt(9);

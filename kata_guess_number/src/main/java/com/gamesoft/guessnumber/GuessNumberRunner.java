dcf4a838a29618c5106e410314547be92ac4225f
zeya <zeyaxp@hotmail.com>
2014-09-27 01:31:16 +0800

    完成主调度函数。主函数直接是代码组装，未采用TDD方式

--- kata_guess_number/src/main/java/com/gamesoft/guessnumber/GuessNumberRunner.java
+++ kata_guess_number/src/main/java/com/gamesoft/guessnumber/GuessNumberRunner.java
@@ -11,10 +11,33 @@ public class GuessNumberRunner {
         System.out.println("welcome to play guess number game! ");
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("please input 4 numbers:");
-        String inputString = sc.nextLine();
-//        System.out.println(“”);
+        GuessNumberGame game = createRandomGuessNumberGame();
+
+        while(game.canTryAgain()) {
+            try {
+                int[] userGuessNumbers = readUserGuessNumbers(sc);
+                System.out.println(game.guess(userGuessNumbers));
+                if (game.isWin()) {
+                    System.out.print("Congratulations, you win !");
+                    return;
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println("wrong input. please try again...");
+            }
+        }
+        System.out.println("Sorry, you lost !");
     }
 
-//    public
+    private static GuessNumberGame createRandomGuessNumberGame() {
+        RandomAnswerGenerator answerGenerator = new RandomAnswerGenerator();
+        GuessNumberGame game = GuessNumberGame.createGuessNumberGame(answerGenerator.randomGenerate());
+        game.setMaxTry(6);
+        return game;
+    }
+
+    private static int[] readUserGuessNumbers(Scanner sc) {
+        System.out.print("please input 4 numbers:");
+        String inputString = sc.nextLine();
+        return StringUtil.convertToIntArray(inputString);
+    }
 }

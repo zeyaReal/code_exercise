2b30c03b99d62ffd961a85c580d0f17e1cfff6ce
zeya <zeyaxp@hotmail.com>
2014-09-28 23:07:20 +0800

    重构

--- kata_tennis/src/main/java/TennisGame.java
+++ kata_tennis/src/main/java/TennisGame.java
@@ -3,7 +3,7 @@
  */
 public class TennisGame {
 
-    private static final String[] SCORE = new String[]{"", "fifteen", "thirty", "forty"};
+    private static final String[] SCORE = new String[]{"love", "fifteen", "thirty", "forty"};
 
     private int leftScore = 0;
     private int rightScore = 0;
@@ -13,10 +13,7 @@ public class TennisGame {
         if (leftScore == rightScore && leftScore == 0)
             return "love all";
 
-        if (leftScore == 0) {
-            return "love"+" "+SCORE[this.rightScore];
-        }
-        return SCORE[this.leftScore] + " "+ "love";
+            return SCORE[this.leftScore]+" "+SCORE[this.rightScore];
     }
 
     public void leftWin() {

1a4c3be0dc5f3c71ceb9c03047549162f5d666ac
zeya <zeyaxp@hotmail.com>
2014-10-09 21:29:40 +0800

    重构测试

--- kata_arab_to_roman/src/test/java/ArabRomanConverterTest.java
+++ kata_arab_to_roman/src/test/java/ArabRomanConverterTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,27 +8,31 @@ import static org.hamcrest.core.Is.is;
  * Created by zeya on 14-10-8.
  */
 public class ArabRomanConverterTest {
+
+    private ArabRomanConverter converter;
+
+    @Before
+    public void setUp() {
+        converter = new ArabRomanConverter();
+    }
+
     @Test
     public void test_should_return_I_when_input_1() throws Exception {
-        ArabRomanConverter converter = new ArabRomanConverter();
         assertThat(converter.convert(1), is("I"));
     }
 
     @Test
     public void test_should_return_II_when_input_2() throws Exception {
-        ArabRomanConverter converter = new ArabRomanConverter();
         assertThat(converter.convert(2), is("II"));
     }
 
     @Test
     public void test_should_return_III_when_input_3() throws Exception {
-        ArabRomanConverter converter = new ArabRomanConverter();
         assertThat(converter.convert(3), is("III"));
     }
 
     @Test
     public void test_should_return_V_when_input_5() throws Exception {
-        ArabRomanConverter converter = new ArabRomanConverter();
         assertThat(converter.convert(5), is("V"));
     }
 

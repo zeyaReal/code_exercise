341546e64783b75bbbec8eb09b01a4528ae442e8
zeya <zeyaxp@hotmail.com>
2014-10-09 21:19:34 +0800

    增加第一个测试并通过

---
+++ kata_arab_to_roman/src/main/java/ArabRomanConverter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zeya on 14-10-8.
+ */
+public class ArabRomanConverter {
+    public String convert(int number) {
+        return "I";
+    }
+}
---
+++ kata_arab_to_roman/src/test/java/ArabRomanConverterTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by zeya on 14-10-8.
+ */
+public class ArabRomanConverterTest {
+    @Test
+    public void test_should_return_I_when_input_1() throws Exception {
+        ArabRomanConverter converter = new ArabRomanConverter();
+        assertThat(converter.convert(1), is("I"));
+    }
+}

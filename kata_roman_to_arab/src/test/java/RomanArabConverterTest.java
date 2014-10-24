411707c9958216eff23e034051a69b8e248bd2e9
zeya <zeyaxp@hotmail.com>
2014-10-24 22:24:11 +0800

    重构测试，参数化改造

--- kata_roman_to_arab/src/test/java/RomanArabConverterTest.java
+++ kata_roman_to_arab/src/test/java/RomanArabConverterTest.java
@@ -1,5 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -7,9 +12,25 @@ import static org.hamcrest.core.Is.is;
 /**
  * Created by zeya on 14-10-24.
  */
+@RunWith(Parameterized.class)
 public class RomanArabConverterTest {
 
     private RomanArabConverter converter;
+    private String romanNumber;
+    private int arabNumber;
+
+    public RomanArabConverterTest(String romanNumber, int arabNumber) {
+        this.romanNumber = romanNumber;
+        this.arabNumber = arabNumber;
+    }
+
+    @Parameterized.Parameters
+    public static Collection romanToArabDCatas() {
+        return Arrays.asList(new Object[][]{
+                {"I", 1},
+                {"II", 2}
+        });
+    }
 
     @Before
     public void setUp() {
@@ -17,12 +38,7 @@ public class RomanArabConverterTest {
     }
 
     @Test
-    public void test_convert_I_to_1() throws Exception {
-        assertThat(1, is(converter.convert("I")));
-    }
-
-    @Test
-    public void test_convert_II_to_2() throws Exception {
-        assertThat(2, is(converter.convert("II")));
+    public void test_convert_romanNumber_to_ArabNumber() throws Exception {
+        assertThat(this.arabNumber, is(converter.convert(this.romanNumber)));
     }
 }

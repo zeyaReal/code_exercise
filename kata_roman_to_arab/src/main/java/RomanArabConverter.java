94d71c1ff2696c6bc974bde0bb5f941d0e22dfb8
zeya <zeyaxp@hotmail.com>
2014-10-24 22:43:01 +0800

    重构

--- kata_roman_to_arab/src/main/java/RomanArabConverter.java
+++ kata_roman_to_arab/src/main/java/RomanArabConverter.java
@@ -1,23 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by zeya on 14-10-24.
  */
 public class RomanArabConverter {
+
+    private Map<String, Integer> romanArabMap = new TreeMap<String, Integer>();
+
+    public RomanArabConverter() {
+        romanArabMap.put("I", 1);
+        romanArabMap.put("IV", 4);
+        romanArabMap.put("V", 5);
+    }
+
     public int convert(String romanNumber) {
 
         if (romanNumber.isEmpty()) {
             return 0;
         }
 
-        if (romanNumber.equals("I")) {
-            return 1;
-        }
-
-        if (romanNumber.equals("IV")) {
-            return 4;
-        }
-
-        if (romanNumber.equals("V")) {
-            return 5;
+        if (romanArabMap.containsKey(romanNumber)){
+            return romanArabMap.get(romanNumber);
         }
 
         return 1 + convert(romanNumber.substring(1));

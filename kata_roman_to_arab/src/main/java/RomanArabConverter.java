9507b9999e03ce8ea4852480d4c0f4659ec143b1
zeya <zeyaxp@hotmail.com>
2014-10-25 23:14:50 +0800

    重构，简化算法

--- kata_roman_to_arab/src/main/java/RomanArabConverter.java
+++ kata_roman_to_arab/src/main/java/RomanArabConverter.java
@@ -21,20 +21,17 @@ public class RomanArabConverter {
 
     public int convert(String romanNumber) {
 
-        if (romanNumber.isEmpty()) {
-            return 0;
-        }
+        int result = 0;
+        int lastRightArabVal = 0;
 
-        int result = romanArabMap.get( romanNumber.charAt(romanNumber.length()-1) );
+        for (int i = romanNumber.length() - 1; i >=0 ; i--) {
+            int currentArabVal = romanArabMap.get( romanNumber.charAt(i) );
 
-        for (int i = 0; i < romanNumber.length() - 1; i++) {
-            int currentNumber = romanArabMap.get( romanNumber.charAt(i) );
-            int rightNumber = romanArabMap.get( romanNumber.charAt(i+1) );
-
-            if (rightNumber > currentNumber) {
-                result -= currentNumber;
+            if (currentArabVal < lastRightArabVal) {
+                result -= currentArabVal;
             } else {
-                result += currentNumber;
+                result += currentArabVal;
+                lastRightArabVal = currentArabVal;
             }
         }
         return  result;
